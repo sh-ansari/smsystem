12f6268c4d0a842e88b35079fac4fb9dbb2cbf71
Saddam Ansari <sh.ansari746@gmail.com>
2024-08-14 19:53:14 +0530

    first commit

---
+++ src/main/java/com/saddam/smsystem/SmsystemApplication.java
@@ -0,0 +1,13 @@
+package com.saddam.smsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmsystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/saddam/smsystem/SmsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saddam.smsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
